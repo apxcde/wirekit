7469d9e5305111e9756c65c0faa0073877be7740
Erick Mwamodo <erick@mwamodo.com>
2025-05-08 03:22:06 +0300

    WIP: custom installation script

--- app/Actions/Commands/KitInstall.php
+++ app/Actions/Commands/KitInstall.php
@@ -3,9 +3,12 @@
 namespace App\Actions\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Lorisleiva\Actions\Concerns\AsAction;
 
-class KitInstall
+use function Laravel\Prompts\confirm;
+
+final class KitInstall
 {
     use AsAction;
 
@@ -13,13 +16,33 @@ class KitInstall
 
     public $commandDescription = 'Run WireKit installation process';
 
-    public function handle()
-    {
-        //
-    }
+    private $initializeGit = false;
 
     public function asCommand(Command $command)
     {
-        $command->info('Running WireKit installation process...');
+        $command->line('');
+        $command->info('Starting WireKit installation process...');
+        $command->line('');
+
+        $this->handle($command);
+    }
+
+    public function handle(Command $command)
+    {
+        $this->handleGitRepository($command);
+    }
+
+    protected function handleGitRepository(Command $command)
+    {
+        $command->info('Checking if git repository exists...');
+        $command->line('');
+
+        if (File::isDirectory(base_path('.git'))) {
+            $command->info('Git repository already exists.');
+            $command->line('');
+            return;
+        }
+
+        $this->initializeGit = confirm('Initialize Git repository after installation?', true);
     }
 }
