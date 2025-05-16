6080d13bd100a51a781965b67eb9303004cf68f7
Erick Mwamodo <erick@mwamodo.com>
2025-05-16 16:02:28 +0300

    Implement LoginForm with Livewire and enhance dark mode design
    Added a Livewire-based LoginForm with authentication and rate-limiting functionality. Enhanced the dark mode UI by tweaking styles in the authentication layout and replacing the font with "Inter" for improved readability. Removed social login options from the login page for a cleaner interface.

--- resources/views/components/layouts/auth/card.blade.php
+++ resources/views/components/layouts/auth/card.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-zinc-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full dark">
 <head>
     @include('partials.head')
 </head>
-    <body class="h-full">
+    <body class="h-full dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-                <div class="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+                <div class="bg-white dark:bg-white/10 border border-zinc-200 dark:border-white/10 px-6 py-12 sm:rounded-xl sm:px-12">
                     {{ $slot }}
                 </div>
             </div>
