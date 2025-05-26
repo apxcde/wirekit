cfe72f7bf91dd220d9edf5d39eed5cae3a83044d
Erick Mwamodo <erick@mwamodo.com>
2025-05-26 19:42:53 +0300

    Add environment setup and project naming in Kit installation
    Refactored the Kit installation command to include environment file setup and project name configuration. Introduced methods to create/update the `.env` file, reload environment variables, and prompt the user for project-specific details such as name and URL. These improvements enhance the setup process for new projects.

--- app/Actions/Commands/Kit/Install.php
+++ app/Actions/Commands/Kit/Install.php
@@ -3,6 +3,8 @@
 namespace App\Actions\Commands\Kit;
 
 use Illuminate\Console\Command;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use Illuminate\Support\Facades\File;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 use function Laravel\Prompts\{confirm, text};
@@ -27,7 +29,73 @@ final class Install
 
     public function handle(Command $command): void
     {
+        $this->setUpEnvFile($command);
+        $this->setProjectName($command);
+        $this->reloadEnvironment();
         $this->runMigrations($command);
+    }
+
+    private function reloadEnvironment(): void
+    {
+        $app = app();
+        $app->bootstrapWith([
+            LoadEnvironmentVariables::class,
+        ]);
+    }
+
+
+    private function updateEnv(string $key, string $value): void
+    {
+        $path = base_path('.env');
+
+        if (File::exists($path)) {
+            file_put_contents($path, preg_replace(
+                "/^{$key}=.*/m",
+                "{$key}=\"{$value}\"",
+                file_get_contents($path)
+            ));
+        }
+    }
+
+    private function setUpEnvFile(Command $command): void
+    {
+        if (! File::exists('.env') && File::exists('.env.example')) {
+            $command->line('Creating the .env file...');
+            File::copy('.env.example', '.env');
+        }
+
+        $envContent = File::get('.env');
+        if (! preg_match('/^APP_ENV=local/m', $envContent)) {
+            $this->updateEnv('APP_ENV', 'local');
+        }
+    }
+
+    private function setProjectName(Command $command): void
+    {
+        if (env('APP_NAME') !== 'WireKit') {
+            $command->line('Project name already set. Skipping.');
+            return;
+        }
+
+        $defaultName = basename(getcwd());
+        $name = text(
+            label: 'What is the name of your project?',
+            placeholder: $defaultName,
+            default: $defaultName,
+            required: true
+        );
+
+        $this->updateEnv('APP_NAME', $name);
+
+        $defaultUrl = "http://{$name}.test";
+        $url = text(
+            label: 'What is the URL of your project?',
+            placeholder: $defaultUrl,
+            default: $defaultUrl,
+            required: true
+        );
+
+        $this->updateEnv('APP_URL', $url);
     }
 
     private function runMigrations(Command $command): void
