d1c39144814e74d59b57a48a4edc6e0ba2126d5c
Erick Mwamodo <erick@mwamodo.com>
2025-06-07 14:35:21 +0300

    Restarting the project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Lorisleiva\Actions\Facades\Actions;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Model::unguard();
-
-        if ($this->app->runningInConsole()) {
-            Actions::registerCommands([
-                'app/Actions/Commands'
-            ]);
-        }
+        //
     }
 }
