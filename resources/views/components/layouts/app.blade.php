e16b730156224b7b06aa8f3f35b2f9258d481e21
Erick Mwamodo <erick@mwamodo.com>
2025-06-08 20:19:02 +0300

    logout user

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,17 @@
+<?php
+
+use App\Actions\Auth\Logout;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public function logout(Logout $logout): void
+    {
+        $logout();
+        $this->redirect('/', navigate: true);
+    }
+};
+
+?>
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
@@ -17,6 +31,10 @@
     @fluxAppearance
 </head>
 <body class="bg-zinc-50 dark:bg-zinc-900">
+
+    @volt('app.logout')
+        <flux:button wire:click='logout' variant="ghost">Logout</flux:button>
+    @endvolt
 
 {{ $slot }}
 
