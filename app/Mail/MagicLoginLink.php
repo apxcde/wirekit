12c83430c15f1d6ccfcff05b3b52e67c5083af46
Erick Mwamodo <erick@mwamodo.com>
2025-06-08 20:53:26 +0300

    FEATURE: send magic login link email

--- app/Mail/MagicLoginLink.php
+++ app/Mail/MagicLoginLink.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
@@ -13,7 +12,6 @@ class MagicLoginLink extends Mailable
 {
     use Queueable;
     use SerializesModels;
-
 
     public function __construct(public string $url){}
 
